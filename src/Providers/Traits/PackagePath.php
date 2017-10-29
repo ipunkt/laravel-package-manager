306bd5f5beaca06a54894961f7fc7e27f226e5a6
rokde <github@mail.robert-kummer.de>
2017-10-29 13:07:23 +0100

    adding a package root path constant

--- src/Providers/Traits/PackagePath.php
+++ src/Providers/Traits/PackagePath.php
@@ -4,22 +4,23 @@ namespace Ipunkt\Laravel\PackageManager\Providers\Traits;
 
 trait PackagePath
 {
-	/**
-	 * give relative path from package root and return absolute path
-	 *
-	 * @param string $relativePath
-	 * @param string $dir
-	 * @return string
-	 */
-	private function packagePath(string $relativePath, string $dir) : string
-	{
-		$relativePath = ltrim(str_replace('/', DIRECTORY_SEPARATOR, $relativePath), DIRECTORY_SEPARATOR);
+    /**
+     * give relative path from package root and return absolute path
+     *
+     * @param string $relativePath
+     * @param string $dir
+     *
+     * @return string
+     */
+    private function packagePath(string $relativePath, string $dir = PACKAGE_ROOT_PATH): string
+    {
+        $relativePath = ltrim(str_replace('/', DIRECTORY_SEPARATOR, $relativePath), DIRECTORY_SEPARATOR);
 
-		return realpath($dir . DIRECTORY_SEPARATOR .
-			implode(DIRECTORY_SEPARATOR, [
-				'..',
-				'..',
-				$relativePath,
-			]));
-	}
+        return realpath($dir . DIRECTORY_SEPARATOR .
+            implode(DIRECTORY_SEPARATOR, [
+                '..',
+                '..',
+                $relativePath,
+            ]));
+    }
 }
---
+++ src/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+if (!defined('PACKAGE_ROOT_PATH')) {
+    define('PACKAGE_ROOT_PATH', '../../../');
+}

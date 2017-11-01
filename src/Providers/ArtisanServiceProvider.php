6ecfc9d933c37927e52c441d5019126be69e5817
Robert Kummer <rok@ipunkt.biz>
2017-11-01 14:27:43 +0100

    fixed artisan service provider

--- src/Providers/ArtisanServiceProvider.php
+++ src/Providers/ArtisanServiceProvider.php
@@ -3,72 +3,74 @@
 namespace Ipunkt\Laravel\PackageManager\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class ArtisanServiceProvider extends ServiceProvider
 {
-    /**
-     * The commands to be registered.
-     *
-     * command[] or
-     * key => command class or register command method
-     *
-     * @var array
-     */
-    protected $commands = [];
+	/**
+	 * The commands to be registered.
+	 *
+	 * command[] or
+	 * key => command class or register command method
+	 *
+	 * @var array
+	 */
+	protected $commands = [];
 
-    /**
-     * Then you need to have the artisan commands all the time the app gets in action you have to change the value of this attribute
-     *
-     * @var bool
-     */
-    protected $registerOnlyForConsole = true;
+	/**
+	 * Then you need to have the artisan commands all the time the app gets in action you have to change the value of this attribute
+	 *
+	 * @var bool
+	 */
+	protected $registerOnlyForConsole = true;
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        if ($this->registerOnlyForConsole && ! $this->app->runningInConsole()) {
-            return;
-        }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		if ($this->registerOnlyForConsole && !$this->app->runningInConsole()) {
+			return;
+		}
 
-        $this->registerCommands($this->commands);
-    }
+		$this->registerCommands($this->commands);
+	}
 
-    /**
-     * registers an array of commands (key is shortname and value is command class
-     * or part of the registerYYYCommand method name)
-     *
-     * @param array $commands
-     *
-     * @throws \Exception
-     */
-    protected function registerCommands(array $commands)
-    {
-        $registeredCommands = [];
-        foreach ($commands as $key => $command) {
-            if (is_numeric($key)) {
-                $key = $command;
-            }
+	/**
+	 * registers an array of commands (key is shortname and value is command class
+	 * or part of the registerYYYCommand method name)
+	 *
+	 * @param array $commands
+	 *
+	 * @throws \Exception
+	 */
+	protected function registerCommands(array $commands)
+	{
+		if (!$this->app->runningInConsole()) {
+			return;
+		}
 
-            $method = "register{$command}";
+		foreach ($commands as $key => $command) {
+			if (is_numeric($key)) {
+				$key = Str::lower(str_replace("\\", '.', $command));
+			}
 
-            try {
-                if (method_exists($this, $method)) {
-                    call_user_func_array([$this, $method], [$key]);
-                } else {
-                    $this->app->singleton($key, function ($app) use ($command) {
-                        return $app->make($command);
-                    });
-                }
-                $registeredCommands[] = $key;
-            } catch (\Exception $e) {
-                throw $e;
-            }
-        }
+			$method = "register{$command}";
 
-        $this->commands($registeredCommands);
-    }
+			try {
+				if (method_exists($this, $method)) {
+					call_user_func_array([$this, $method], [$key]);
+				} else {
+					$this->app->singleton($key, function ($app) use ($command) {
+						return $app->make($command);
+					});
+				}
+				$this->commands($key);
+			} catch (\Exception $e) {
+				throw $e;
+			}
+		}
+	}
 }
